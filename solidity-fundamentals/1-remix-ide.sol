d62092896f697d8987b597c9bdee8b6763669832
lemusosa <alvinandwealth@gmail.com>
2023-10-25 22:01:47 +0200

    add in solidity fundamentals directory for tracking learnings - remix section complete

---
+++ solidity-fundamentals/1-remix-ide.sol
@@ -0,0 +1,4 @@
+// https://remix.ethereum.org/
+// remix is a an ide. predominantly used online to write smart contract code in solidity and viper.
+// tips: use auto compile.
+// think of remix as a sandbox for writing and debugging smart contract code.
