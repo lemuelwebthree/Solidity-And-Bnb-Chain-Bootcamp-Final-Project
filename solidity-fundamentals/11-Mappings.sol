907738b465901c0c30346d7b7600f7c950a30e4c
lemusosa <alvinandwealth@gmail.com>
2023-10-27 07:30:11 +0200

    complete amppings illustrating CRUD operations - now move onto structs

--- solidity-fundamentals/11-Mappings.sol
+++ solidity-fundamentals/11-Mappings.sol
@@ -1,4 +1,26 @@
 // SPDX-License-Identifier: MIT
-// compiler version mhas to be greater than or equal (>=) to 0.8.20 and
+// compiler version mhas to be greater than or equal (>=) to 0.8.20 and 
 // less than 0.9.0
 pragma solidity ^0.8.20;
+
+contract Mappings {
+    // nams mapping - mapping address keys to value names
+    // use public access modifier to auot-geerate respctive getter
+    // note that the types for mapping keys can only be built in types - however
+    // related values of key-value pairs can be custom data types like a Struct
+    // of perosn details (name and age) to store in a mapping called friends list.
+    // CREATE
+    mapping(address => string) public names;
+    // READ
+    function getName() public view returns (string memory) {
+        return names[msg.sender];
+    }
+    // UPDATE
+    function setName(string memory _name) public {
+        names[msg.sender] = _name;
+    }
+    // DELETE
+    function deleteMappingKV() public {
+        delete names[msg.sender];
+    }
+}
