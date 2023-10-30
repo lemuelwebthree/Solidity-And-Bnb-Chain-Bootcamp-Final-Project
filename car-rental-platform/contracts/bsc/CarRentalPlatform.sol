1145a1d3f3839c161431d3b3e7da98945caa455a
lemusosa <alvinandwealth@gmail.com>
2023-10-30 15:00:46 +0200

    ad din some implementation code - constrcutor has totalPayments initialised to zero

--- car-rental-platform/contracts/bsc/CarRentalPlatform.sol
+++ car-rental-platform/contracts/bsc/CarRentalPlatform.sol
@@ -7,31 +7,72 @@ pragma solidity ^0.8.4;
 // to reduce errors.
 // 1. Logic outline: contains an outline of data types required, the functions needed
 
-import "../../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CarRentalPlatform {
   //1. DATA SECTION
 
   //Counter
+  using Counters for Counters.Counter;
+  Counters.COunter private_counter;
 
   //Owner
+  address private owner;
 
   //totalPayments
+  uint private totalPayments;
 
   //user struct
+  struct User {
+    address walletAddress;
+    string name;
+    string lastname;
+    uint rentedCarId;
+    uint balance;
+    uint debt;
+    uint start;
+  }
 
   //car struct
+  struct Car {
+    uint id;
+    string name;
+    string imgUrl;
+    Status status;
+    uint rentFee;
+    uint saleFee;
+  }
 
   //enum to inidcate the of the car
-
+  enum Status {
+    Retired,
+    InUse,
+    Available
+  }
   // events
+  event CarAdded(uint indexed id, string name, string imgUrl, uint rentFee, uint saleFee);
+  event CarMetadataEdited(uint indexed id, string name, string imgUrl, uint rentFee, uint saleFee);
+  event CarStatusEdited(uint indexed id, string name, Status status);
+  event UserAdded(uint indexed walletAddresss, string name, string lastname);
+  event Deposit(uint indexed walletAddress, uint amount);
+  event CheckOut(address indexed walletAddress, uint indexed carId);
+  event CheckIn(uint indexed walletAddress, uint indexed carId);
+  event PaymentMade(address indexed walletAddress, uint amount);
+  event BalanceWithdrawn(address indexed walletAddress, uint amount);
 
-  //user ampping
+
+  //user mapping
+  mapping(address => User) private users;
 
   //car mapping
+  mapping(uint => Car) private cars;
 
   //constructor
-
+  constructor() {
+    owner = msg.sender;
+    //initialise total payments to zero
+    totalPayments = 0;
+  }
   //2. MODIFIFIERS SECTION
 
   //onlyOwner 
