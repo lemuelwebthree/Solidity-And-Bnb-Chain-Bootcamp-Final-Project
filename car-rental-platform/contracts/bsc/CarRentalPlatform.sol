700f9093e876b9a8872ff7f81094fa445fd96a91
lemusosa <alvinandwealth@gmail.com>
2023-10-31 12:15:29 +0200

    car rental project successful compilation

--- car-rental-platform/contracts/bsc/CarRentalPlatform.sol
+++ car-rental-platform/contracts/bsc/CarRentalPlatform.sol
@@ -25,7 +25,7 @@ contract CarRentalPlatform is ReentrancyGuard {
   uint private totalPayments;
 
   //user struct
-  struct User {
+  struct User{
     address walletAddress;
     string name;
     string lastname;
@@ -36,7 +36,7 @@ contract CarRentalPlatform is ReentrancyGuard {
   }
 
   //car struct
-  struct Car {
+  struct Car{
     uint id;
     string name;
     string imgUrl;
@@ -51,23 +51,24 @@ contract CarRentalPlatform is ReentrancyGuard {
     InUse,
     Available
   }
+
   // events
-  event CarAdded(uint indexed id, string name, string imgUrl, uint rentFee, uint saleFee);
+  event CarAdded(uint indexed id, string name, string imgUrl,uint rentFee, uint saleFee);
   event CarMetadataEdited(uint indexed id, string name, string imgUrl, uint rentFee, uint saleFee);
-  event CarStatusEdited(uint indexed id, string name, Status status);
-  event UserAdded(uint indexed walletAddresss, string name, string lastname);
-  event Deposit(uint indexed walletAddress, uint amount);
+  event CarStatusEdited(uint indexed id, Status status);
+  event UserAdded(address indexed walletAddress, string name, string lastname);
+  event Deposit(address indexed walletAddress, uint amount);
   event CheckOut(address indexed walletAddress, uint indexed carId);
-  event CheckIn(uint indexed walletAddress, uint indexed carId);
+  event CheckIn(address indexed walletAddress, uint indexed carId);
   event PaymentMade(address indexed walletAddress, uint amount);
   event BalanceWithdrawn(address indexed walletAddress, uint amount);
 
 
   //user mapping
-  mapping(address => User) private users;
+  mapping(address => User ) private users;
 
   //car mapping
-  mapping(uint => Car) private cars;
+   mapping(uint => Car) private cars;
 
   //constructor
   constructor() {
@@ -80,7 +81,7 @@ contract CarRentalPlatform is ReentrancyGuard {
   //onlyOwner 
   modifier onlyOwner() {
     require(msg.sender == owner, "Only the owner can call this function");
-    // do not forget to add in merge wildcard in modifier definitions
+    // merge wildcard
     _;
   }
 
@@ -88,7 +89,7 @@ contract CarRentalPlatform is ReentrancyGuard {
   //3.1. Execute Functions
 
   //setOwner #onlyOwner
-  function setOwner(address _newOwner) external onlyOwner {
+  function setOwner(address _newOwner) external onlyOwner{
     owner = _newOwner;
   }
 
@@ -111,24 +112,23 @@ contract CarRentalPlatform is ReentrancyGuard {
   }
 
   //editCarStatus #onlyOwner #exisingCar
-  function editCarMetadata(uint id, string calldata name, string calldata imgUrl, uint rentFee, uint saleFee) external onlyOwner {
-      // cannot edit a card that does not exist
-      require(cars[id].id !=0, "Car with given ID does not exist");
-      Car storage car = cars[id];
-      if(bytes(name).length !=0) {
-        car.name = name;
-      }
-      if(bytes(imgUrl).length !=0) {
-        car.imgUrl = imgUrl;
-      }
-      if(rentFee > 0) {
-        car.rentFee = rentFee;
-      }
-      if(saleFee > 0) {
-        car.saleFee = saleFee;
-      }
+  function editCarMetadata(uint id, string calldata name, string calldata imgUrl, uint rentFee, uint saleFee) external onlyOwner{
+    require(cars[id].id !=0, "Car with given ID does not exist");
+    Car storage car = cars[id];
+    if(bytes(name).length != 0){
+      car.name = name;
+    }
+    if(bytes(imgUrl).length != 0){
+      car.imgUrl = imgUrl;
+    }
+    if(rentFee > 0){
+      car.rentFee = rentFee;
+    }
+    if(saleFee > 0){
+      car.saleFee = saleFee;
+    }
 
-      emit CarMetadataEdited(id, car.name, car.imgUrl, car.rentFee, car.saleFee);
+    emit CarMetadataEdited(id, car.name, car.imgUrl, car.rentFee, car.saleFee);
   }
 
 
@@ -141,17 +141,17 @@ contract CarRentalPlatform is ReentrancyGuard {
   }
 
   //checkOut #exitingUser #isCarAvailable #userHasNotRentedACar #userHasNoDebt
-  function checkOut(uint id) external {
-    require(isUser(msg.sender), "User does not exist");
-    require(cars[id].status == Status.Available, "Car is not Available for use");
-    require(users[msg.sender].rentedCarId == 0, "User has already rented a car");
-    require(users[msg.sender].debt == 0, "User has an outstanding debt");
+  function checkOut(uint id) external{
+      require(isUser(msg.sender), "User does not exist");
+      require(cars[id].status == Status.Available, "Car is not Available for use");
+      require(users[msg.sender].rentedCarId == 0, "User has already rented a car");
+      require(users[msg.sender].debt == 0, "User has an outstanding debt!");
 
-    users[msg.sender].start = block.timestamp;
-    users[msg.sender].rentedCarId = id;
-    cars[id].status = Status.InUse;
+      users[msg.sender].start = block.timestamp;
+      users[msg.sender].rentedCarId = id;
+      cars[id].status = Status.InUse;
 
-    emit CheckOut(msg.sender, id);
+      emit CheckOut(msg.sender,id);
   }
 
   //checkIn #exisintgUser #userHasRentedACar
@@ -172,7 +172,7 @@ contract CarRentalPlatform is ReentrancyGuard {
   }
 
   //deposit #existingUser
-  function deposit() external payable {
+  function deposit() external payable{
     require(isUser(msg.sender), "User does not exist");
     users[msg.sender].balance += msg.value;
 
@@ -180,13 +180,13 @@ contract CarRentalPlatform is ReentrancyGuard {
   }
 
   //makePayment #existingUser #existingDebt #sufficientBalance
-  function makePayment() external {
+  function makePayment() external{
     require(isUser(msg.sender), "User does not exist");
     uint debt = users[msg.sender].debt;
     uint balance = users[msg.sender].balance;
 
-    require(debt > 0, "User has no debt to pay");
-    require(balance >= debt, "User has insuficient balance");
+    require(debt > 0 , "User has no debt to pay");
+    require(balance >= debt, "User has insufficient balance");
 
     unchecked {
       users[msg.sender].balance -= debt;
@@ -196,27 +196,28 @@ contract CarRentalPlatform is ReentrancyGuard {
 
     emit PaymentMade(msg.sender, debt);
   }
+
   //withdrawalBalance #existingUser with nonReentrant
-  function withdrawBalance(uint amount) external nonReentrant {
+  function withdrawBalance(uint amount) external nonReentrant{
     require(isUser(msg.sender), "User does not exist");
     uint balance = users[msg.sender].balance;
     require(balance >= amount, "Insufficient balance to withdraw");
 
     unchecked {
       users[msg.sender].balance -= amount;
-    }
+    } 
 
-    (bool success,) = msg.sender.call{value: amount}("");
+    (bool success, ) = msg.sender.call{value: amount}("");
     require(success, "Transfer failed");
 
     emit BalanceWithdrawn(msg.sender, amount);
   }
 
   //withdrawOwnerBalance #onlyOwner
-  function withdrawOwnerBalance(uint amount) external onlyOwner {
-    require(totalPayments >= amount, "Insufficient contract balance to withdraw");
+  function withdrawOwnerBalane(uint amount) external onlyOwner{
+    require(totalPayments >= amount, "Insufficient Contract Balance to withdraw");
 
-    (bool success,) = owner.call{value: amount}("");
+    (bool success, ) = owner.call{value: amount}("");  
     require(success, "Transfer failed");
 
     unchecked {
@@ -227,23 +228,23 @@ contract CarRentalPlatform is ReentrancyGuard {
   //3.2. Query Functions
 
   //getOwner
-  function getOwner() private view returns(address) {
+  function getOwner() external view returns(address){
     return owner;
   }
 
   //isUser
-  function isUser(address walletAddress) private view returns(bool) {
+  function isUser(address walletAddress) private view returns(bool){
     return users[walletAddress].walletAddress != address(0);
   }
 
   //getUser #existingUser
-  function getUser(address walletAddress) external view returns(User memory) {
-    require(isUser(walletAddress), "User does not exist");
+  function getUser(address walletAddress) external view returns(User memory){
+    require(isUser(walletAddress), "Wallet Address does not exist");
     return users[walletAddress];
   }
 
   //getCar #existingCar
-  function getCar(uint id) external view returns(Car memory) {
+ function getCar(uint id) external view returns (Car memory){
     require(cars[id].id != 0, "Car does not exist");
     return cars[id];
   }
@@ -270,24 +271,23 @@ contract CarRentalPlatform is ReentrancyGuard {
   }
 
   //calculateDebt
-  function calculateDebt(uint usedSeconds, uint rentFee) private pure returns(uint) {
-    // convert to seconds
+  function calculateDebt(uint usedSeconds, uint rentFee) private pure returns(uint){
     uint usedMinutes = usedSeconds / 60;
     return usedMinutes * rentFee;
   }
 
   //getCurrentCount
-  function getCurrentCount() external view returns(uint) {
+  function getCurrentCount() external view returns(uint){
     return _counter.current();
   }
 
   //getContractBalance #onlyOwner
-  function getContractBalance() external view onlyOwner returns(uint) {
+  function getContractBalance() external view onlyOwner returns(uint){
     return address(this).balance;
   }
 
   //getTotalPayment #onlyOwner
-  function getTotalPayment() external view onlyOwner returns(uint) {
+  function getTotalPayments() external view onlyOwner returns(uint){
     return totalPayments;
   }
 
