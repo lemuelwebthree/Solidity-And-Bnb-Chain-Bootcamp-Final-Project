0e789a65099842ca07d32805e49199cd7b11eb25
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 12:54:05 +0200

    Update 8-Functions.sol

--- solidity-fundamentals/8-Functions.sol
+++ solidity-fundamentals/8-Functions.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// compiler version mhas to be greater than or equal (>=) to 0.8.20 and 
+// compiler version has to be greater than or equal (>=) to 0.8.20 and 
 // less than 0.9.0
 pragma solidity ^0.8.20;
 
@@ -17,7 +17,7 @@ contract Functions {
 
     // pure functions are similar to view fucntions in a way
     // but they are more restricted. Does not interact with blockchain directly
-    // and it allows us to perfrom arbitary calculcations as an example
+    // and it allows us to perfrom arbitary calculations as an example
     function add(uint a, uint b) public pure returns (uint) {
         return a + b;
     }
