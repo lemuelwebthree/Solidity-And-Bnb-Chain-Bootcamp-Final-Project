302ce7f288561882ab4b0a90c0a970038a4eca55
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 12:48:21 +0200

    Update 4-Address.sol

--- solidity-fundamentals/4-Address.sol
+++ solidity-fundamentals/4-Address.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 contract Address {
     // getter function will not be automatically generated for 
     // private state variables
-    // use msg.sender to get owner address instead of hardcoding it
+    // use msg.sender ( a global variable) to get owner address instead of hardcoding it
     address private owner = msg.sender;
 
     function getOwner() public view returns(address) {
