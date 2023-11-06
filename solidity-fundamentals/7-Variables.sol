a4aa745c7986ab2eda931d61be0cfd7e83bb31d6
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 12:52:25 +0200

    Update 7-Variables.sol

--- solidity-fundamentals/7-Variables.sol
+++ solidity-fundamentals/7-Variables.sol
@@ -4,11 +4,11 @@
 pragma solidity ^0.8.20;
 
 contract Variables {
-    // state variabes are contract scope
+    // state variables are contract scope
     string public myStateVariable = "State Variable";
 
     // local variables are decalred in a fucntion - they are function scoped
-    // variables with visibility "restricted" to a function
+    // variables with visibility "restricted" to a function definition
     function add(uint a) public pure returns (uint) {
         uint localUint = 10;
         return localUint + a;
