30f20a40c6bd9bb351efb586874275e073e65013
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 12:58:36 +0200

    Update 13-Modifiers.sol

--- solidity-fundamentals/13-Modifiers.sol
+++ solidity-fundamentals/13-Modifiers.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// compiler version mhas to be greater than or equal (>=) to 0.8.20 and 
+// compiler version has to be greater than or equal (>=) to 0.8.20 and 
 // less than 0.9.0
 pragma solidity ^0.8.20;
 
@@ -12,12 +12,12 @@ contract ModifierExample {
         myNumber = _myNumber;
     }
 
-    // modifier is an automatic condition checker that can added
-    // into function signature.
+    // modifier is an automatic condition checker that can be added
+    // into a function signature.
     // Modifiers can be created with parameters or not.
     // if conditions are met then the function will execute.
-    // think of modifiers as function-execution gatekeeper used
-    // to create "permissioned" function sin solidity. Makes sense?
+    // think of modifiers as function-execution gatekeepers used
+    // to create "permissioned" functions in solidity. Makes sense?
     // Great!
     modifier onlyOwnerOrAddress(address _allowedAddress) {
         require(msg.sender == owner || msg.sender == _allowedAddress, "Only owner or allowed address can call this function");
