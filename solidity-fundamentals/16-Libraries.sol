a172d57f13d676ab2789125a1ad232147f8e1af7
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 13:00:29 +0200

    Update 16-Libraries.sol

--- solidity-fundamentals/16-Libraries.sol
+++ solidity-fundamentals/16-Libraries.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-// compiler version mhas to be greater than or equal (>=) to 0.8.20 and 
+// compiler version has to be greater than or equal (>=) to 0.8.20 and 
 // less than 0.9.0
 pragma solidity ^0.8.20;
 
-// lirbaries can be used on pure and view functions to reuse code time and time agin
-// by importing the library and making use of a specific functions or set of functions
-// that get used frequently. You can have a library for concatenations a 
+// libraries can be used on pure and view functions to reuse code time and time again
+// by importing the library and making use of a specific function or set of functions
+// that get used frequently. You can have a library for concatenations 
 library MyLibrary {
     function double(uint[] memory nums) public pure returns(uint[] memory) {
         uint[] memory result = new uint[](nums.length);
