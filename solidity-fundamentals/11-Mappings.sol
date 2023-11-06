9c191d535ded61cfbed9be5a737ea3093e11a638
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 12:56:50 +0200

    Update 11-Mappings.sol

--- solidity-fundamentals/11-Mappings.sol
+++ solidity-fundamentals/11-Mappings.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
-// compiler version mhas to be greater than or equal (>=) to 0.8.20 and 
+// compiler version has to be greater than or equal (>=) to 0.8.20 and 
 // less than 0.9.0
 pragma solidity ^0.8.20;
 
 contract Mappings {
-    // nams mapping - mapping address keys to value names
-    // use public access modifier to auot-geerate respctive getter
+    // names mapping - mapping address keys to value names
+    // use public access modifier to auto-generate respective getters
     // note that the types for mapping keys can only be built in types - however
     // related values of key-value pairs can be custom data types like a Struct
-    // of perosn details (name and age) to store in a mapping called friends list.
+    // of person details (name and age) to store in a mapping called friends list.
 
     // NOTES ON DOUBLE MAPPINGS OR NESTED MAPPINGS OR HASHMAPS
-    // Nested mapping are not really gas efficient but they ar efor complex
-    // dtata types however they are not iterable
+    // Nested mappings are not really gas efficient but they are for complex
+    // data types however they are not iterable
 
     // CREATE
     mapping(address => string) public names;
