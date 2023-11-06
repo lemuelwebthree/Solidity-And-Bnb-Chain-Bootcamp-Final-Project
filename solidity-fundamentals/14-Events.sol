53a933b94c40ae1cb98ba2309f0cb45b36df0114
Solomon WE₿3 <124439840+lemusosa@users.noreply.github.com>
2023-11-06 12:59:29 +0200

    Update 14-Events.sol

--- solidity-fundamentals/14-Events.sol
+++ solidity-fundamentals/14-Events.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-// compiler version mhas to be greater than or equal (>=) to 0.8.20 and
+// compiler version has to be greater than or equal (>=) to 0.8.20 and
 // less than 0.9.0
 pragma solidity ^0.8.20;
 
-// events help us nto notify external parties about state changes within
-// asmart contract. evnts get activated or trigger emit.
-// When events are emitted, they are added to a log entry in the transaction rceipt,
+// events help us to notify external parties about state changes within
+// a smart contract. events get activated or trigger emit.
+// When events are emitted, they are added to a log entry in the transaction receipt,
 // which can be read by external applications and used to triggger further actions
 contract Events {
     event NewTransaction(uint indexed transactionId, address indexed sender, address indexed recipient, uint amount);
